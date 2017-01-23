07d553450f5c046a018549db1752eec429747e3b
Mark Tomlinson <mark_tomlinson@uk.ibm.com>
2017-01-23 13:20:39 +0000

    added ejs

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ var app = express();
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
+// added by MRT to add ejs view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
@@ -26,4 +30,12 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+/* ----------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
+
+app.get('/', function(req, res){
+  res.render('home');
 });
